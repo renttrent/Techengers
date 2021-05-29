873a770d028d44c266f6dae4b1cf63069575279e
NikaelaBalla <nikaela109@gmail.com>
2021-05-29 16:09:35 +0200

    models

--- Code/gym_project/dashboards/admin.py
+++ Code/gym_project/dashboards/admin.py
@@ -1,3 +1,8 @@
+from .models import DietPlan, Event, Exercise, Routine
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(DietPlan)
+admin.site.register(Exercise)
+admin.site.register(Routine)
--- Code/gym_project/dashboards/models.py
+++ Code/gym_project/dashboards/models.py
@@ -1,3 +1,44 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class DietPlan(models.Model):
+    diet_name = models.CharField(max_length = 30)
+    diet_dscr = models.TextField()
+    last_modified = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.diet_name
+
+class Event(models.Model) :
+    event_name = models.CharField(max_length=60)
+    pub_date = models.DateTimeField('date published')
+    event_dscr = models.TextField()
+
+    def __str__(self):
+        return self.event_name
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Exercise(models.Model) :
+    exercise_name = models.CharField(max_length=60)
+    exercise_dscr = models.TextField()
+    img = models.ImageField(upload_to = "images/")
+
+    def __str__(self):
+        return self.exercise_name
+
+class Routine(models.Model) :
+    routime_name = models.CharField(max_length=60)
+    routime_dscr = models.TextField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.routime_name
+
+  
+
+
+
