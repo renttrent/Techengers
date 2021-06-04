4d3af91a321cc87a68d3b9b56c587ac9efe013fd
renttrent <37352219+renttrent@users.noreply.github.com>
2021-06-04 20:41:53 +0200

    Added User Profile model

--- Code/gym_project/dashboards/models.py
+++ Code/gym_project/dashboards/models.py
@@ -2,43 +2,41 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class DietPlan(models.Model):
-    diet_name = models.CharField(max_length = 30)
+    diet_name = models.CharField(max_length=30)
     diet_dscr = models.TextField()
-    last_modified = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.diet_name
+        return f"Diet Plan - {self.diet_name}"
 
-class Event(models.Model) :
+
+class Event(models.Model):
     event_name = models.CharField(max_length=60)
     pub_date = models.DateTimeField('date published')
     event_dscr = models.TextField()
 
     def __str__(self):
-        return self.event_name
+        return f"Event - {self.event_name}"
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Exercise(models.Model) :
+
+class Exercise(models.Model):
     exercise_name = models.CharField(max_length=60)
     exercise_dscr = models.TextField()
-    img = models.ImageField(upload_to = "images/")
+    exercise_thumbnail = models.ImageField(upload_to="media")
 
     def __str__(self):
-        return self.exercise_name
+        return f"Exercise - {self.exercise_name}"
 
-class Routine(models.Model) :
-    routime_name = models.CharField(max_length=60)
-    routime_dscr = models.TextField()
+
+class Routine(models.Model):
+    routine_name = models.CharField(max_length=60)
+    routine_dscr = models.TextField()
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.routime_name
-
-  
-
-
-
+        return f"Routine - {self.routine_name}"
--- Code/gym_project/gym_project/urls.py
+++ Code/gym_project/gym_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('login.urls')),
     path('', include('dashboards.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
