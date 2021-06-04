4bf45f8035196f61f25fd95e009a22c1ad59fd25
renttrent <37352219+renttrent@users.noreply.github.com>
2021-06-04 20:15:22 +0200

    User Profile Model

--- Code/gym_project/login/models.py
+++ Code/gym_project/login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    height = models.FloatField()
+    weight = models.FloatField()
+    weight_goal = models.FloatField()
