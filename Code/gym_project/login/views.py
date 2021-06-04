1fa1281533d3434fa2cef7e3c5e19bafba20fafc
renttrent <37352219+renttrent@users.noreply.github.com>
2021-06-04 21:02:04 +0200

    small changes

--- Code/gym_project/login/views.py
+++ Code/gym_project/login/views.py
@@ -3,11 +3,18 @@ from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
-
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Loggin In {username}')
+            return redirect('userdashboard')
     return render(request, 'login/login.html')
 
 
@@ -19,7 +26,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'Account created successfully. You can Log In.')
+                request, f'Account created successfully. You can Log In {username}.')
 
             return redirect('login')
     else:
