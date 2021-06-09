d2b653fe835fb0564ee91e50a928228f34629266
renttrent <37352219+renttrent@users.noreply.github.com>
2021-06-10 00:10:02 +0200

    added profile menu - added scrollbar style

---
+++ Code/gym_project/static/js/profile-menu.js
@@ -0,0 +1,11 @@
+// alert("test");
+// console.log("test");
+// $(() => {
+//   $(".user-btn").click((e) => {
+//     alert("test");
+//   });
+
+//   $(".three-dots").click((e) => {
+//     alert("dots");
+//   });
+// });
