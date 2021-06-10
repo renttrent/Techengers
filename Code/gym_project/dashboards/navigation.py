225306670b229d06d2f1493258b0e91d7130e970
renttrent <37352219+renttrent@users.noreply.github.com>
2021-06-10 14:38:10 +0200

    staff dashboard changes

---
+++ Code/gym_project/dashboards/navigation.py
@@ -0,0 +1,135 @@
+SCHEDULE_NAV = navigation = {
+    'schedule': {
+        'name': 'Schedule',
+        'isActive': True,
+        'url': 'schedule'
+    },
+    'exercises': {
+        'name': 'Exercises',
+        'isActive': False,
+        'url': 'exercises'
+    },
+    'routines': {
+        'name': 'Routines',
+        'isActive': False,
+        'url': 'routines'
+    },
+    'diets': {
+        'name': 'Selected Diets',
+        'isActive': False,
+        'url': 'diets'
+    },
+    'trainers': {
+        'name': 'Trainers',
+        'isActive': False,
+        'url': 'trainers'
+    },
+}
+EXERCISES_NAV = navigation = {
+    'schedule': {
+        'name': 'Schedule',
+        'isActive': False,
+        'url': 'schedule'
+    },
+    'exercises': {
+        'name': 'Exercises',
+        'isActive': True,
+        'url': 'exercises'
+    },
+    'routines': {
+        'name': 'Routines',
+        'isActive': False,
+        'url': 'routines'
+    },
+    'diets': {
+        'name': 'Selected Diets',
+        'isActive': False,
+        'url': 'diets'
+    },
+    'trainers': {
+        'name': 'Trainers',
+        'isActive': False,
+        'url': 'trainers'
+    },
+}
+ROUTINES_NAV = navigation = {
+    'schedule': {
+        'name': 'Schedule',
+        'isActive': False,
+        'url': 'schedule'
+    },
+    'exercises': {
+        'name': 'Exercises',
+        'isActive': False,
+        'url': 'exercises'
+    },
+    'routines': {
+        'name': 'Routines',
+        'isActive': True,
+        'url': 'routines'
+    },
+    'diets': {
+        'name': 'Selected Diets',
+        'isActive': False,
+        'url': 'diets'
+    },
+    'trainers': {
+        'name': 'Trainers',
+        'isActive': False,
+        'url': 'trainers'
+    },
+}
+DIETS_NAV = navigation = {
+    'schedule': {
+        'name': 'Schedule',
+        'isActive': False,
+        'url': 'schedule'
+    },
+    'exercises': {
+        'name': 'Exercises',
+        'isActive': False,
+        'url': 'exercises'
+    },
+    'routines': {
+        'name': 'Routines',
+        'isActive': False,
+        'url': 'routines'
+    },
+    'diets': {
+        'name': 'Selected Diets',
+        'isActive': True,
+        'url': 'diets'
+    },
+    'trainers': {
+        'name': 'Trainers',
+        'isActive': False,
+        'url': 'trainers'
+    },
+}
+TRAINERS_NAV = navigation = {
+    'schedule': {
+        'name': 'Schedule',
+        'isActive': False,
+        'url': 'schedule'
+    },
+    'exercises': {
+        'name': 'Exercises',
+        'isActive': False,
+        'url': 'exercises',
+    },
+    'routines': {
+        'name': 'Routines',
+        'isActive': False,
+        'url': 'routines'
+    },
+    'diets': {
+        'name': 'Selected Diets',
+        'isActive': False,
+        'url': 'diets'
+    },
+    'trainers': {
+        'name': 'Trainers',
+        'isActive': True,
+        'url': 'trainers'
+    },
+}
